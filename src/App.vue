e7d038d68574bd0f473fe7578b5e3e61e5c81b0f
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2020-09-05 23:24:02 +0300

    Add new files.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+
+      <button class="btn">test</button>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+    @import '~materialize-css/dist/css/materialize.min.css';
+    @import "assets/index.css";
+</style>
